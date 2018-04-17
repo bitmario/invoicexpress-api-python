41fd00b46d507a411b4b8f47db0d2454771b1613
Mário <26830122+bitmario@users.noreply.github.com>
2018-04-17 16:41:34 +0100

    Bum version to 0.1.1

--- invoicexpress_api/__init__.py
+++ invoicexpress_api/__init__.py
@@ -4,7 +4,7 @@ Client library for the InvoiceXpress REST API
 
 __author__ = """Mario Falcao"""
 __email__ = 'mario@bitsiders.com'
-__version__ = '0.1.0'
+__version__ = '0.1.1'
 
 from .core import Client
 from . import clients
--- setup.py
+++ setup.py
@@ -35,6 +35,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/bitmario/invoicexpress-api-python',
-    version='0.1.0',
+    version='0.1.1',
     zip_safe=False,
 )
