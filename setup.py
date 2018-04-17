1446a3597d2e34808d26af44745956e99d500972
Mário <26830122+bitmario@users.noreply.github.com>
2018-04-17 06:28:11 +0100

    Fix requirements

--- setup.py
+++ setup.py
@@ -5,9 +5,9 @@ from setuptools import setup, find_packages
 with open('README.rst') as readme_file:
     readme = readme_file.read()
 
-requirements = [ ]
+requirements = [ 'requests' ]
 
-setup_requirements = [ 'requests' ]
+setup_requirements = [ ]
 
 test_requirements = [ ]
 
