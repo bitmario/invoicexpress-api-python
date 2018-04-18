b9d381781af2307df416469226e6916015b53955
Mário <26830122+bitmario@users.noreply.github.com>
2018-04-18 01:57:31 +0100

    Improve tests and travis config

--- tests/test_core.py
+++ tests/test_core.py
@@ -1,26 +1,33 @@
+from pytest import fixture
 from invoicexpress_api import Client
 
 
-def test_build_params():
-    c = Client('https://dummy.app.invoicexpress.com', 'secret123')
-    params = c._build_params({'a': 1, 'x': 'asd'})
+@fixture
+def client():
+    return Client('https://dummy.app.invoicexpress.com', 'secret123')
+
+
+def test_build_params(client):
+    params = client._build_params({'a': 1, 'x': 'asd'})
 
     assert len(params) == 3
+    assert 'a' in params
     assert params['a'] == 1
+    assert 'x' in params
     assert params['x'] == 'asd'
+    assert 'api_key' in params
     assert params['api_key'] == 'secret123'
 
 
-def test_build_params_empty():
-    c = Client('https://dummy.app.invoicexpress.com', 'secret123')
-    params = c._build_params({})
+def test_build_params_empty(client):
+    params = client._build_params({})
     
     assert len(params) == 1
+    assert 'api_key' in params
     assert params['api_key'] == 'secret123'
 
 
-def test_build_url():
-    c = Client('https://dummy.app.invoicexpress.com', 'secret123')
-    url = c._build_url('/test.json')
+def test_build_url(client):
+    url = client._build_url('/test.json')
 
     assert url == 'https://dummy.app.invoicexpress.com/test.json'
